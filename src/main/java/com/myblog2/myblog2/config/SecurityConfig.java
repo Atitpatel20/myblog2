75b042cbbdf36e833f74d94cb32c4516433c1969
Atit Patel <atitpatel296@gmail.com>
2024-02-14 19:00:03 +0530

    127:developed sign in feature with spring security 09.02.2024

--- src/main/java/com/myblog2/myblog2/config/SecurityConfig.java
+++ src/main/java/com/myblog2/myblog2/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package com.myblog2.myblog2.config;
 
+import com.myblog2.myblog2.security.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,7 +22,8 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+    @Autowired
+    private CustomUserDetailsService userDetailsService;
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -47,4 +52,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .encode("admin")).roles("ADMIN").build();
         return new InMemoryUserDetailsManager(user1, user2);
     }
+
+    @Bean // Expose AuthenticationManager as a bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
 }
--- src/main/java/com/myblog2/myblog2/controller/AuthController.java
+++ src/main/java/com/myblog2/myblog2/controller/AuthController.java
@@ -2,12 +2,17 @@ package com.myblog2.myblog2.controller;
 
 import com.myblog2.myblog2.entity.Role;
 import com.myblog2.myblog2.entity.User;
+import com.myblog2.myblog2.payload.LoginDto;
 import com.myblog2.myblog2.payload.SignUpDto;
 import com.myblog2.myblog2.repository.RoleRepository;
 import com.myblog2.myblog2.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +32,18 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/signup")
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/signin")
+    public ResponseEntity<String> authenticateUser(@RequestBody LoginDto loginDto) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getUsernameOrEmail(), loginDto.getPassword());
+        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return new ResponseEntity<>("User signed-in successfully!.", HttpStatus.OK);
+    }
+        @PostMapping("/signup")
     public ResponseEntity<?> createRegistration(@RequestBody SignUpDto signUpDto) {
         if (userRepository.existsByEmail(signUpDto.getEmail())) {
             return new ResponseEntity<>("Email is already exists", HttpStatus.BAD_REQUEST);
